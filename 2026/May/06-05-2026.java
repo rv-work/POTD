35b02c763d37bbd0628dc83ef1bd591ff62c2d2b
rv-work <rv.dthought@gmail.com>
2026-05-06 19:14:09 +0530

    POTD : 06-05-2026

---
+++ 2026/May/06-05-2026.java
@@ -0,0 +1,34 @@
+
+
+
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            int emptySlot = n - 1; 
+            
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    emptySlot = j - 1;
+
+
+                } else if (box[i][j] == '#') {
+                    box[i][j] = '.'; 
+                    box[i][emptySlot] = '#'; 
+                    emptySlot--; 
+                }
+            }
+        }
+        
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = box[i][j];
+            }
+        }
+        
+        return res;
+    }
+}
